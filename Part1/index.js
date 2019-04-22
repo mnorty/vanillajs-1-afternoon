13b56e8bfaaade7017b1ac004a0be0eb38f11ccd
mnorty <mnorty@gmail.com>
2019-04-22 14:33:34 -0600

    adding square names

--- Part1/index.js
+++ Part1/index.js
@@ -8,11 +8,25 @@ function play(clicked){
         playerSpan.innerText = 'O'
         boxClicked.innerText = "X"
         progress[clicked] = 'X'
-    }
-    else {playerSpan.innerText = 'X'
+        // winCheck()
+    }else {
+        playerSpan.innerText = 'X'
         boxClicked.innerText = "O"
         progress[clicked] = 'O'
     }
     console.log(progress)
+    // let winCheck(){
+    //     if( progress === progress['X','X','X'])
+    // }
+    let topLeft = progress[0]
+    let topMiddle = progress[1]
+    let topRight = progress[2]
+    let middleLeft = progress[3]
+    let middleMiddle = progress[4]
+    let middleRight = progress[5]
+    let bottomLeft = progress[6]
+    let bottomMiddle = progress[7]
+    let bottomRight = progress[8]
 }
-       
+
+// window.alert("yo!")       
